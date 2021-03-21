15cacdad5bad1d171f040dce981a931a499f543a
Ray <balafranzraymond@gmail.com>
2021-03-21 21:20:56 +0800

    destructuring-rest-spread

---
+++ master-class/restParameters-spreadOperator-destructuring.js
@@ -0,0 +1,55 @@
+// rest parameter
+function sum(...thisArgs) {
+	// arguments object cannot use array functions
+	return thisArgs.reduce(function (previous, current) {
+		return previous + current;
+	});
+}
+
+console.log(sum(5, 5, 5));
+
+// spread operator - object
+const userDetails = {
+	name: "Rysh",
+	address: "Anywhere",
+	sample: {
+		test: "test",
+	},
+};
+
+const newUserDetails = { ...userDetails, mobileNumber: 123 };
+
+newUserDetails.sample.test = "newTest";
+
+console.log("userDetails:", userDetails);
+console.log("newUserDetails:", newUserDetails);
+
+// spread operator - array
+const fruits = ["apple", "avocado"];
+
+const newFruits = [...fruits, "guava"];
+
+console.log("fruits:", fruits);
+console.log("newFruits:", newFruits);
+
+// destructuring - object
+const userDetails = {
+	name: "Rysh",
+	address: "Anywhere",
+	sample: {
+		test: "test",
+	},
+};
+
+const { name: newName = "May", ...otherDetails } = userDetails;
+
+console.log(newName);
+console.log(otherDetails);
+
+// destructuring - array
+const fruits = ["avocado", "guava", "coconut"];
+
+const [firstFruit = "strawberry", ...otherFruits] = fruits;
+
+console.log(firstFruit);
+console.log(otherFruits);
