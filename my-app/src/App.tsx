a5c0cf8bdb0a2f3a1a4b4073663ce358a59093ed
Ray <balafranzraymond@gmail.com>
2021-03-25 23:33:13 +0800

    :fire: 35Lifecycle - Mounting & Unmounting hands-on

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,13 +2,29 @@ import React from "react";
 import "./App.css";
 import CounterManagement from "./components/CounterManagement";
 
-function App() {
-	return (
-		<>
-			<h1>My React App!</h1>
-			<CounterManagement ownerName='CanonMinds' />
-		</>
-	);
+interface AppState {
+	change: boolean;
+}
+class App extends React.Component<{}, AppState> {
+	constructor(props: {}) {
+		super(props);
+		this.state = {
+			change: true,
+		};
+	}
+	clickButton = () => {
+		this.setState({ change: !this.state.change });
+	};
+
+	render() {
+		return (
+			<>
+				<h1>My React App!</h1>
+				{this.state.change && <CounterManagement ownerName='CanonMinds' />}
+				<button onClick={this.clickButton}>Change</button>
+			</>
+		);
+	}
 }
 
 export default App;
--- my-app/src/components/CounterManagement/index.tsx
+++ my-app/src/components/CounterManagement/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { CounterManagementProps, CounterManagementState } from "./interface";
+import axios from "axios";
 
 class CounterManagement extends React.Component<
 	CounterManagementProps,
@@ -10,7 +11,9 @@ class CounterManagement extends React.Component<
 
 		this.state = {
 			counter: 0,
+			users: [],
 		};
+		console.log("constructor");
 	}
 
 	handleAddClick = () => {
@@ -30,9 +33,39 @@ class CounterManagement extends React.Component<
 		this.setState({ counter: this.state.counter - 1 });
 	};
 
+	static getDerivedStateFromProps(
+		props: CounterManagementProps,
+		state: CounterManagementState
+	) {
+		console.log("getDerivedStateFromProps");
+		// Used in rare cases only.
+		return null;
+	}
+
+	clickWindow = () => {
+		console.log("clickWindow Event Occur.");
+		this.setState({ counter: this.state.counter + 1 });
+	};
+
+	componentDidMount() {
+		console.log("Component did mount");
+		axios.get("https://reqres.in/api/users?page=2").then((response) => {
+			const data = response.data;
+			const users = data.data.map((userData: any) => userData.first_name);
+			this.setState({ users });
+		});
+
+		window.addEventListener("click", this.clickWindow);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener("click", this.clickWindow);
+	}
+
 	render() {
+		console.log("render");
 		const { ownerName } = this.props;
-		const { counter } = this.state;
+		const { counter, users } = this.state;
 		return (
 			<div>
 				<h1> Counter Management </h1>
@@ -40,6 +73,13 @@ class CounterManagement extends React.Component<
 				<h3> Counter: {counter} </h3>
 				<button onClick={this.handleAddClick}>Add</button>
 				<button onClick={this.handleSubtractClick}>Subtract</button>
+				<div>
+					<ul>
+						{users.map((user) => (
+							<li>{user}</li>
+						))}
+					</ul>
+				</div>
 			</div>
 		);
 	}
